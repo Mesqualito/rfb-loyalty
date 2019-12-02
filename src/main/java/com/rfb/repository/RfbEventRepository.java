801370dbcbb976add0f1336999af7f1c6bd55a7c
Mesqualito <jochen@gebsattel.rocks>
2019-12-02 16:41:08 +0100

    Use of Spring '@Scheduled' for a service, that inserts a code for every new event in a location. This code will be given from the organisers to the runner to control attendance. Also two new test-classes test the RfbEventRepository and the RfbLocationRepository

--- src/main/java/com/rfb/repository/RfbEventRepository.java
+++ src/main/java/com/rfb/repository/RfbEventRepository.java
@@ -1,15 +1,21 @@
 package com.rfb.repository;
 
 import com.rfb.domain.RfbEvent;
+import com.rfb.domain.RfbLocation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+
 
 /**
- * Spring Data  repository for the RfbEvent entity.
+ * Spring Data JPA repository for the RfbEvent entity.
  */
 @SuppressWarnings("unused")
 @Repository
 public interface RfbEventRepository extends JpaRepository<RfbEvent, Long> {
 
+    // Spring Data will handle the implementation:
+    // Spring Data will form the query and return back the RfbEvent-Object
+    RfbEvent findByRfbLocationAndEventDate(RfbLocation location, LocalDate date);
 }
