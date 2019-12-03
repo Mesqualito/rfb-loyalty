1ceaf626ba511e8f403f1d0884322b4ee9c5c9df
Mesqualito <jochen@gebsattel.rocks>
2019-12-03 13:37:09 +0100

    JHipster Angular default Frontend starts with index.html located in /src/main/webapp and the loading pac-man (as well as handling of old browser and js-check), linking /src/main/webapp/app/content/css/loading.css and bootstrapping 'JhiMainComponent' located in /src/main/webapp/app/layouts/main/main.component.ts with '<jhi-main>'.
    main.component.html starts to link the components together:
    - jhi-page-ribbon
    - router-outlet name="navbar"
    - router-outlet
    - router-outlet name="popup"
    - jhi-footer
    
    In index.html, Fonts can be linked (e.g.: '<link href="https://fonts.googleapis.com/css?family=Walter+Turncoat" rel="stylesheet">') and used in the css-files of the respective component.

--- src/main/webapp/app/layouts/footer/footer.component.ts
+++ src/main/webapp/app/layouts/footer/footer.component.ts
@@ -4,4 +4,10 @@ import { Component } from '@angular/core';
   selector: 'jhi-footer',
   templateUrl: './footer.component.html'
 })
-export class FooterComponent {}
+export class FooterComponent {
+  cuttingYears: number;
+
+  constructor() {
+    this.cuttingYears = new Date().getFullYear() - 1921;
+  }
+}
