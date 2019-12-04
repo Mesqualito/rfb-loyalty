813686d7880ff5753fe37a124ddbf7b1d9ea2d86
Mesqualito <jochen@gebsattel.rocks>
2019-12-04 18:34:14 +0100

    Security Roles - change ROLE_ADMIN to ROLE_ROOT
    IMPORTANT:
    - get users with passwords in the database: "Register" first and change roles with "admin" before changing authority-configuration in Spring backend!
    - configure Spring backend first and check all menu- / navbar-items and API for "Error Page! You are not authorized to access this page." in the browser (see this commit)
    - then change Angular frontend according to your backend rules (next commit)
    
    refers to #3

--- src/main/java/com/rfb/config/SecurityConfiguration.java
+++ src/main/java/com/rfb/config/SecurityConfiguration.java
@@ -126,7 +126,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .antMatchers("/management/health").permitAll()
             .antMatchers("/management/info").permitAll()
             .antMatchers("/management/prometheus").permitAll()
-            .antMatchers("/management/**").hasAuthority(AuthoritiesConstants.ROOT);
+            //.antMatchers("/management/**").hasAuthority(AuthoritiesConstants.ROOT)
+        ;
         // @formatter:on
     }
 }
